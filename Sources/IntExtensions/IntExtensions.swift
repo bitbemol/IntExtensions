c16709dfc349f85958a78d8384243c23e5fc10fc
Bemol <110437038+bitbemol@users.noreply.github.com>
2024-03-04 15:53:59 -0600

    Basic extenions

--- Sources/IntExtensions/IntExtensions.swift
+++ Sources/IntExtensions/IntExtensions.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+extension Int {
+    var isEven: Bool {
+        self.isMultiple(of: 2)
+    }
+    
+    var isOdd: Bool {
+        !self.isMultiple(of: 2)
+    }
+}
--- Tests/IntExtensionsTests/IntExtensionsTests.swift
+++ Tests/IntExtensionsTests/IntExtensionsTests.swift
@@ -2,11 +2,19 @@ import XCTest
 @testable import IntExtensions
 
 final class IntExtensionsTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testIsEvenTrue() {
+        XCTAssertEqual(2.isEven, true)
+    }
+    
+    func testIsEvenFalse() {
+        XCTAssertEqual(3.isEven, false)
+    }
+    
+    func testIsOddTrue() {
+        XCTAssertEqual(3.isOdd, true)
+    }
+    
+    func testIsOddFalse() {
+        XCTAssertEqual(2.isOdd, false)
     }
 }
